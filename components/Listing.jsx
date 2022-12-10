405e1964f65e66d69789f5ab5f01de48914dcf27
rifatptk <rifatptk@gmail.com>
2022-12-10 19:03:30 +0600

    listings updated

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -4,26 +4,26 @@ import ListingCard from './ListingCard';
 
 const Listing = () => {
   const [data, setdata] = useState([]);
-  const [loading, setloading] = useState(true);
-  const [limit, setlimit] = useState(20);
+  const [loading, setloading] = useState(false);
   const [page, setpage] = useState(1);
 
   useEffect(() => {
+    setloading(true);
     axios
-      .get(`http://localhost:4000/listing?page=${page}&limit=${limit}`)
+      .get(`http://localhost:4000/listing?page=${page}`)
       .then((res) => {
         setdata(res.data);
         setloading(false);
       })
       .catch((err) => console.log(err));
-  }, [limit, page]);
+  }, [page]);
 
   return (
     <>
       <h1 className="text-2xl font-bold text-gray-700 my-5">Listings</h1>
 
       {loading ? (
-        <h1>Loading...</h1>
+        <h1>Loading... </h1>
       ) : (
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
           {data?.map((el, i) => (
@@ -34,35 +34,18 @@ const Listing = () => {
       <br />
 
       <div className="my-5 py-5 text-center border-t">
-        <div className="w-fit mx-auto mb-5">
-          <h3 className="text-gray-600 font-bold mb-2">Data per page</h3>
-          <div className="flex gap-4">
-            {[10, 20, 50, 100].map((el, i) => (
-              <div
-                key={i}
-                onClick={() => setlimit(el)}
-                className={`rounded bg-blue-500 hover:bg-blue-400 text-white font-bold px-3 cursor-pointer py-1 ${
-                  el === limit ? 'bg-gray-400' : ''
-                }`}
-              >
-                {el}
-              </div>
-            ))}
-          </div>
-        </div>
-
         <div className="w-fit mx-auto">
           <h3 className="text-gray-600 font-bold mb-2">Page No</h3>
           <div className="flex gap-4 flex-wrap justify-center">
-            {new Array(500 / limit).fill(null).map((el, i) => (
+            {new Array(10).fill(null).map((el, i) => (
               <div
                 key={i}
-                onClick={() => setpage(i)}
+                onClick={() => setpage(i + 1)}
                 className={`rounded bg-blue-500 hover:bg-blue-400 text-white font-bold px-3 cursor-pointer py-1 ${
-                  i === page ? 'bg-gray-400' : ''
+                  i + 1 === page ? 'bg-gray-400' : ''
                 }`}
               >
-                {i}
+                {i + 1}
               </div>
             ))}
           </div>
